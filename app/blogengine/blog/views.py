cb452bfbacba08a1712302ef1958bf2cb2f614b4
Hamersly <maksimwanderer96@gmail.com>
2019-06-03 00:14:55 +0500

    Добавлена постраничная навигация

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -8,12 +8,35 @@ from .forms import TagForm, PostForm
 from django.shortcuts import redirect
 # Ниже, что бы скрыть от неавторизованных пользователей что-либо.
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 def posts_list(request):
     posts = Post.objects.all()
-    return render(request, 'blogg/index.html', context={'posts': posts})
+    paginator = Paginator(posts, 2)
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous():
+        prv_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prv_url = ''
+
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ''
+
+    context = {
+        'page_object': page,
+        'is_paginated': is_paginated,
+        'next_url': next_url,
+        'prv_url': prv_url,
+    }
+
+    return render(request, 'blogg/index.html', context=context)
 
 
 class PostDetail(ObjectDetailMixin, View):
@@ -21,18 +44,20 @@ class PostDetail(ObjectDetailMixin, View):
     template = 'blogg/post_detail.html'
 
 
-class PostCreate(LoginRequiredMixin,ObjectCreateMixin, View):
+class PostCreate(LoginRequiredMixin, ObjectCreateMixin, View):
     form_model = PostForm
     template = 'blogg/post_create.html'
     raise_exception = True
 
-class PostUpdate(LoginRequiredMixin,ObjectUpdateMixin, View):
+
+class PostUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
     model = Post
     form_model = PostForm
     template = 'blogg/post_update.html'
     raise_exception = True
 
-class PostDelete(LoginRequiredMixin,ObjectDeleteMixin,View):
+
+class PostDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
     model = Post
     template = 'blogg/post_delete.html'
     redirect_url = 'posts_list_url'
@@ -49,18 +74,20 @@ class TagDetail(ObjectDetailMixin, View):
     template = 'blogg/tag_detail.html'
 
 
-class TagCreate(LoginRequiredMixin,ObjectCreateMixin, View):
+class TagCreate(LoginRequiredMixin, ObjectCreateMixin, View):
     form_model = TagForm
     template = 'blogg/tag_create.html'
     raise_exception = True
 
-class TagUpdate(LoginRequiredMixin,ObjectUpdateMixin, View):
+
+class TagUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
     model = Tag
     form_model = TagForm
     template = 'blogg/tag_update.html'
     raise_exception = True
 
-class TagDelete(LoginRequiredMixin,ObjectDeleteMixin,View):
+
+class TagDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
     model = Tag
     template = 'blogg/tag_delete.html'
     redirect_url = 'tags_list_url'
